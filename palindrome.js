2f66073c0dcb6414cdd79379cf2e42ae931e228f
unknown <milan9427@gmail.com>
2022-12-01 18:24:01 +0530

    problems

---
+++ palindrome.js
@@ -0,0 +1,22 @@
+function checkPalindrome(N, str) {
+    let bag1="";
+    let bag2="";
+    for(let i=0;i<str.length;i++)
+    {
+        bag1=bag1+str[j];
+    }
+    for(let j=str.length-1;j>=0;j--)
+    {
+        bag2=bag2+str[j];
+    }
+    
+    if(bag1==bag2)
+    {
+        console.log("Yes");
+    }
+    else
+    {
+        console.log("No");
+    }
+  
+}
---
+++ prime.js
@@ -0,0 +1,20 @@
+function identifyPrime(num) {
+        let count=0;
+        for(let n=1;n<=num;n++)
+        {
+            if(num%n===0)
+            {
+                count;
+            }
+        }
+        if(count==2)
+        {
+            console.log("Yes");
+        }
+        else
+        {
+            console.log("No");
+        }
+        
+           
+}
